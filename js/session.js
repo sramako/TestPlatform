cd94886e338306ea8eec30efa55c1cab3ea3403d
Sraman <sramakoo@gmail.com>
2019-05-11 17:56:38 +0530

    Beta Test Release

--- js/session.js
+++ js/session.js
@@ -19,117 +19,117 @@ function getCookie(cname) {
 	}
 	return "";
 }
-
-function login(x,y) {
-	// $.post("server/login.php",
-	$.post("https://ppdb-ep.herokuapp.com/emplogin",
-	{
-					"username": x,
-					"password": y
-	},
-	function (data) {
-		console.log("+"+JSON.stringify(data)+"+");
-		console.log(data["isAdmin"]);
-		if(data["isAdmin"]==true){
-			setCookie("admin","true",1);
-		}
-		else{
-			setCookie("admin","false",1);
-		}
-
-		if(data["status"]=="success") {
-			console.log("Login Success");
-			setCookie("login", "true", 1);
-			setCookie("user", x, 1);
-			window.location.href="announcements.html";
-		}
-		else {
-			console.log("Login Failed");
-			setCookie("login", "false", 1);
-			swal("Wrong Credentials","error");
-			return "false";
-		}
-	})}
-
-	function login_admin(x,y) {
-		// $.post("server/login.php",
-		$.post("https://ppdb-ep.herokuapp.com/emplogin",
-		{
-						"username": x,
-						"password": y
-		},
-		function (data) {
-			console.log("+"+JSON.stringify(data)+"+");
-			console.log(data["isAdmin"]);
-			if(data["isAdmin"]==true){
-				setCookie("admin","true",1);
-			}
-			else{
-				setCookie("admin","false",1);
-			}
-
-			if(data["status"]=="success") {
-				console.log("Login Success");
-				setCookie("login", "true", 1);
-				setCookie("user", x, 1);
-				window.location.href="newemp.html";
-			}
-			else {
-				console.log("Login Failed");
-				setCookie("login", "false", 1);
-				swal("Wrong Credentials","error");
-				return "false";
-			}
-		})}
-
-
-
-	function check_login() {
-		c=getCookie("login");
-		u=getCookie("user");
-		console.log(c);
-		console.log(u);
-		if(c!="true") {
-			window.stop();
-			swal("You're not logged in!","error");
-			window.location.href="employee_login.html";
-			return false;
-		}
-		return u;
-	}
-
-	function check_login_admin() {
-		c=getCookie("login");
-		u=getCookie("user");
-		a=getCookie("admin");
-		console.log(c);
-		console.log(u);
-		if(c!="true") {
-			window.stop();
-			swal("You're not logged in!","error");
-			window.location.href="admin_login.html";
-			return false;
-		}
-		if(a!="true"){
-			window.stop();
-			swal("You're not logged in as admin","error");
-			window.location.href="admin_login.html";
-			return false;
-		}
-		return u;
-	}
-
-
-
-	function signout() {
-		setCookie("login", "false", 1);
-		setCookie("admin", "false", 1);
-		check_login();
-		window.location.href="https://sramako.github.io";
-	}
-
-	function signout_admin() {
-		setCookie("login", "false", 1);
-		setCookie("admin", "false", 1);
-		window.location.href="admin_login.html";
-	}
+//
+// function login(x,y) {
+// 	// $.post("server/login.php",
+// 	$.post("https://ppdb-ep.herokuapp.com/emplogin",
+// 	{
+// 					"username": x,
+// 					"password": y
+// 	},
+// 	function (data) {
+// 		console.log("+"+JSON.stringify(data)+"+");
+// 		console.log(data["isAdmin"]);
+// 		if(data["isAdmin"]==true){
+// 			setCookie("admin","true",1);
+// 		}
+// 		else{
+// 			setCookie("admin","false",1);
+// 		}
+//
+// 		if(data["status"]=="success") {
+// 			console.log("Login Success");
+// 			setCookie("login", "true", 1);
+// 			setCookie("user", x, 1);
+// 			window.location.href="announcements.html";
+// 		}
+// 		else {
+// 			console.log("Login Failed");
+// 			setCookie("login", "false", 1);
+// 			swal("Wrong Credentials","error");
+// 			return "false";
+// 		}
+// 	})}
+//
+// 	function login_admin(x,y) {
+// 		// $.post("server/login.php",
+// 		$.post("https://ppdb-ep.herokuapp.com/emplogin",
+// 		{
+// 						"username": x,
+// 						"password": y
+// 		},
+// 		function (data) {
+// 			console.log("+"+JSON.stringify(data)+"+");
+// 			console.log(data["isAdmin"]);
+// 			if(data["isAdmin"]==true){
+// 				setCookie("admin","true",1);
+// 			}
+// 			else{
+// 				setCookie("admin","false",1);
+// 			}
+//
+// 			if(data["status"]=="success") {
+// 				console.log("Login Success");
+// 				setCookie("login", "true", 1);
+// 				setCookie("user", x, 1);
+// 				window.location.href="newemp.html";
+// 			}
+// 			else {
+// 				console.log("Login Failed");
+// 				setCookie("login", "false", 1);
+// 				swal("Wrong Credentials","error");
+// 				return "false";
+// 			}
+// 		})}
+//
+//
+//
+// 	function check_login() {
+// 		c=getCookie("login");
+// 		u=getCookie("user");
+// 		console.log(c);
+// 		console.log(u);
+// 		if(c!="true") {
+// 			window.stop();
+// 			swal("You're not logged in!","error");
+// 			window.location.href="employee_login.html";
+// 			return false;
+// 		}
+// 		return u;
+// 	}
+//
+// 	function check_login_admin() {
+// 		c=getCookie("login");
+// 		u=getCookie("user");
+// 		a=getCookie("admin");
+// 		console.log(c);
+// 		console.log(u);
+// 		if(c!="true") {
+// 			window.stop();
+// 			swal("You're not logged in!","error");
+// 			window.location.href="admin_login.html";
+// 			return false;
+// 		}
+// 		if(a!="true"){
+// 			window.stop();
+// 			swal("You're not logged in as admin","error");
+// 			window.location.href="admin_login.html";
+// 			return false;
+// 		}
+// 		return u;
+// 	}
+//
+//
+//
+// 	function signout() {
+// 		setCookie("login", "false", 1);
+// 		setCookie("admin", "false", 1);
+// 		check_login();
+// 		window.location.href="https://sramako.github.io";
+// 	}
+//
+// 	function signout_admin() {
+// 		setCookie("login", "false", 1);
+// 		setCookie("admin", "false", 1);
+// 		window.location.href="admin_login.html";
+// 	}
